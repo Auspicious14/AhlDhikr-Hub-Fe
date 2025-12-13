dcbbb5dab161372fb819a4cfa85a7d30ddc01b28
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-12-13 12:01:40 +0100

    Update GlobalProvider.tsx

--- src/lib/GlobalProvider.tsx
+++ src/lib/GlobalProvider.tsx
@@ -2,6 +2,9 @@ import React, { ReactNode } from "react";
 import { AskProvider } from "@/modules/ask/context";
 import { MyDhikrProvider } from "@/modules/my-dhikr/context";
 import { SettingsProvider } from "@/modules/settings/context";
+import { AuthProvider } from "@/modules/auth/context";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 
 interface GlobalProviderProps {
   children: ReactNode;
@@ -9,10 +12,14 @@ interface GlobalProviderProps {
 
 export const GlobalProvider: React.FC<GlobalProviderProps> = ({ children }) => {
   return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+     <AuthProvider>
     <SettingsProvider>
       <MyDhikrProvider>
         <AskProvider>{children}</AskProvider>
       </MyDhikrProvider>
     </SettingsProvider>
+  </AuthProvider>
+</GoogleOAuthProvider>
   );
 };
